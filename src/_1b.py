0c9694ee0061ace38427a383582ae9e1236b8ae4
Andrew Barnett <Abarn279@gmail.com>
2018-12-01 14:27:05 -0600

    used itertools for loop cycle

--- src/_1b.py
+++ src/_1b.py
@@ -1,20 +1,17 @@
 from file_importer import FileImporter
+import itertools
 
 inp = list(map(int, FileImporter.get_input("/../input/1.txt").split("\n")))
 
 freqset = set([0]) 
 _sum = 0
-i = 0
 
-while True:
-    _sum += inp[i]
+for i in itertools.cycle(inp):
+    _sum += i
 
     if _sum in freqset:
         print(_sum)
         break
 
     freqset.add(_sum)
-
-    i = (i + 1) % len(inp)
-
-    
+   
