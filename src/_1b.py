972e49511f56dc0b34e0887d78706ef526eec833
Andrew Barnett <Abarn279@gmail.com>
2018-12-01 14:18:51 -0600

    Corrected files

---
+++ src/_1b.py
@@ -0,0 +1,22 @@
+from file_importer import FileImporter
+
+inp = list(map(int, FileImporter.get_input("/../input/1.txt").split("\n")))
+
+freqset = set([0]) 
+_sum = 0
+i = 0
+
+while True:
+    _sum += inp[i]
+
+    if _sum in freqset:
+        print(_sum)
+        break
+
+    freqset.add(_sum)
+
+    i += 1
+    if i == len(inp):
+        i = 0
+
+    
