4aaf48a6f741563b18ef24324da02b753849c5c7
Andrew Barnett <andrew@legacyesports.gg>
2018-12-03 14:44:26 -0600

    removed unnecessary code

--- src/_3b.py
+++ src/_3b.py
@@ -28,7 +28,6 @@ claim_strs = FileImporter.get_input("/../input/3.txt").split("\n")
 
 claims = get_claims(claim_strs)                                     # List of claims
 grid = [['.' for i in range(1000)] for j in range(1000)]            # 1000x1000 grid
-overlap_count = 0                                                   # Count of total overlapping. Increment each time we place an X
 
 for claim in claims:
     for row in range(claim.y, claim.y + claim.height):
