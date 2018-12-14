a05c7a14a7cccd099334f9e20e5339ece78f655a
Andrew Barnett <Abarn279@gmail.com>
2018-12-14 14:46:18 -0600

    14b

--- src/14b.py
+++ src/14b.py
@@ -1,6 +1,6 @@
 from file_importer import FileImporter
 
-n_recipes = "607331" #FileImporter.get_input("/../input/14.txt")
+n_recipes = FileImporter.get_input("/../input/14.txt")
 
 scoreboard = ['3', '7']
 elf_1_recipe_i = 0
@@ -9,10 +9,10 @@ elf_2_recipe_i = 1
 get_new_recipes = lambda a, b: list(str(int(a) + int(b)))
 
 len_last_chars = len(n_recipes)
-while scoreboard[-len_last_chars:] != list(str(n_recipes)):
+while scoreboard[-len_last_chars:] != list(str(n_recipes)) and scoreboard[-(len_last_chars+1):-1] != list(str(n_recipes)):
     scoreboard += get_new_recipes(scoreboard[elf_1_recipe_i], scoreboard[elf_2_recipe_i])
 
     elf_1_recipe_i = (elf_1_recipe_i + 1 + int(scoreboard[elf_1_recipe_i])) % len(scoreboard)
     elf_2_recipe_i = (elf_2_recipe_i + 1 + int(scoreboard[elf_2_recipe_i])) % len(scoreboard)
 
-print(len(scoreboard) - len_last_chars)
+print(len(scoreboard) - len_last_chars) # Actual answer for my input is -= 1, because 2 were added to scoreboard in that iteration. 
