26edae95127122b1e0d87a90ea0b7b774a895714
Andrew Barnett <andrew@legacyesports.gg>
2018-12-17 12:57:11 -0600

    16a done

--- src/16a.py
+++ src/16a.py
@@ -1,41 +1,67 @@
 from file_importer import FileImporter
-
-inp = FileImporter.get_input("/../input/16.txt").split("\n")
+import re
 
 class Op:
     def __init__(self, op, a, b, c):
         self.op = op; self.a = a; self.b = b; self.c = c
 
+# Addition
+def addr(op, regs): regs[op.c] = regs[op.a] + regs[op.b]; return regs
+def addi(op, regs): regs[op.c] = regs[op.a] + op.b; return regs
+
+# Multiplication
+def mulr(op, regs): regs[op.c] = regs[op.a] * regs[op.b]; return regs
+def muli(op, regs): regs[op.c] = regs[op.a] * op.b; return regs
+
+# Bitwise AND
+def banr(op, regs): regs[op.c] = regs[op.a] & regs[op.b]; return regs
+def bani(op, regs): regs[op.c] = regs[op.a] & op.b; return regs
+
+# Bitwise OR
+def borr(op, regs): regs[op.c] = regs[op.a] | regs[op.b]; return regs
+def bori(op, regs): regs[op.c] = regs[op.a] | op.b; return regs
+
+# Assignment
+def setr(op, regs): regs[op.c] = regs[op.a]; return regs
+def seti(op, regs): regs[op.c] = op.a; return regs
+
+# Greater-than testing:
+def gtir(op, regs): regs[op.c] = 1 if op.a > regs[op.b] else 0; return regs
+def gtri(op, regs): regs[op.c] = 1 if regs[op.a] > op.b else 0; return regs
+def gtrr(op, regs): regs[op.c] = 1 if regs[op.a] > regs[op.b] else 0; return regs
+
+# Equality testing
+def eqir(op, regs): regs[op.c] = 1 if op.a == regs[op.b] else 0; return regs
+def eqri(op, regs): regs[op.c] = 1 if regs[op.a]  == op.b else 0; return regs
+def eqrr(op, regs): regs[op.c] = 1 if regs[op.a] == regs[op.b] else 0; return regs
+
 opcodes = { 
-    # Addition
-    'addr' : lambda op, regs: regs[op.c] = regs[op.a] + regs[op.b],
-    'addi' : lambda op, regs: regs[op.c] = regs[op.a] + op.b,
-
-    # Multiplication
-    'mulr' : lambda op, regs: regs[op.c] = regs[op.a] * regs[op.b],
-    'muli' : lambda op, regs: regs[op.c] = regs[op.a] * op.b,
-
-    # Bitwise AND
-    'banr' : lambda op, regs: regs[op.c] = regs[op.a] & regs[op.b]
-    'bani' : lambda op, regs: regs[op.c] = regs[op.a] & op.b
-
-    # Bitwise OR
-    'borr' : lambda op, regs: regs[op.c] = regs[op.a] | regs[op.b]
-    'bori' : lambda op, regs: regs[op.c] = regs[op.a] | op.b
-
-    # Assignment
-    'setr' : lambda op, regs: regs[op.c] = regs[op.a]
-    'seti' : lambda op, regs: regs[op.c] = op.addi
-
-    # Greater-than testing:
-    'gtir' : lambda op, regs: regs[op.c] = 1 if op.a > regs[op.b] else 0
-    'gtri' : lambda op, regs: regs[op.c] = 1 if regs[op.a] > op.b else 0
-    'gtrr' : lambda op, regs: regs[op.c] = 1 if regs[op.a] > regs[op.b] else 0
-
-    # Equality testing: 
-    'eqir' : lambda op, regs: regs[op.c] = 1 if op.a == regs[op.b] else 0
-    'eqri' : lambda op, regs: regs[op.c] = 1 if regs[op.a]  == op.b else 0
-    'eqrr' : lambda op, regs: regs[op.c] = 1 if regs[op.a] == regs[op.b] else 0
+    'addr' : addr,
+    'addi' : addi,
+    'mulr' : mulr,
+    'muli' : muli,
+    'banr' : banr,
+    'bani' : bani,
+    'borr' : borr,
+    'bori' : bori,
+    'setr' : setr,
+    'seti' : seti,
+    'gtir' : gtir,
+    'gtri' : gtri,
+    'gtrr' : gtrr,
+    'eqir' : eqir,
+    'eqri' : eqri,
+    'eqrr' : eqrr
 }
 
+inp = FileImporter.get_input("/../input/16.txt").split("\n\n\n")[0].split('\n')
+count = 0
+for i in range(0, len(inp), 4):
+    registers_before = eval(re.match('Before: (\[.+\])', inp[i]).groups()[0])
+    op = Op(*map(int, inp[i + 1].split(' ')))
+    registers_after = eval(re.match('After:  (\[.+\])', inp[i + 2]).groups()[0]) 
 
+    if sum(1 for opcode in opcodes if opcodes[opcode](op, registers_before[:]) == registers_after) >= 3:
+        count += 1
+
+print(count)
