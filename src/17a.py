c097b0e4c60d845f3c64fa11b3ec64d2ac70f500
Andrew Barnett <Abarn279@gmail.com>
2018-12-19 15:34:03 -0600

    17a updates

--- src/17a.py
+++ src/17a.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 from enum import Enum
 from queue import Queue
 import re
+import time
 
 class DIRECTIONS(Enum):
     UP = Vector2(0, -1)
@@ -37,19 +38,39 @@ ymin = min(grid.keys(), key = lambda x: x[0])[0]
 xmax = max(grid.keys(), key = lambda x: x[1])[1]
 xmin = min(grid.keys(), key = lambda x: x[1])[1]
 
+COUNT_WATER = 0
+
+def print_grid_around_cursor(grid, cursor):
+    print('\n'*50)
+    for y in range(cursor.y - 50, cursor.y + 50):
+        for x in range(cursor.x - 50, cursor.x + 50):
+            print(grid[(y, x)], end="")
+        print()
+
 def print_grid(grid):
-    for y in range(ymin, ymax+1):
-        for x in range(xmin, xmax+1):
+    print('\n'*50)
+    for y in range(ymin, ymax + 1):
+        for x in range(xmin, xmax + 1):
             print(grid[(y, x)], end="")
         print()
 
 def fill_left_right_get_new_cursors(grid, cursor): 
     ''' Fill the spaces to the left/right, return an array of new cursors '''
-    left_cursor = cursor + DIRECTIONS.LEFT
-    right_cursor = cursor + DIRECTIONS.RIGHT
+    global COUNT_WATER
+
+    left_cursor = cursor + DIRECTIONS.LEFT.value
+    right_cursor = cursor + DIRECTIONS.RIGHT.value
+
+    # Cursors that were created as a part of this process. 
+    # Cursors are created when water flows off an edge.
     new_cursors = []
+
     while True:
         left_grid_item = grid[left_cursor.to_yx_tuple()]
+
+        if left_grid_item == '|':
+            left_cursor = left_cursor + DIRECTIONS.LEFT.value
+            continue
 
         # Wall
         if left_grid_item == '#':
@@ -57,65 +78,102 @@ def fill_left_right_get_new_cursors(grid, cursor):
 
         # Open space
         if left_grid_item == '.':
-            under_left_grid_item = grid[(left_cursor + DIRECTIONS.DOWN).to_yx_tuple()]
+            under_left_grid_item = grid[(left_cursor + DIRECTIONS.DOWN.value).to_yx_tuple()]
 
-            if under_left_grid_item == '~':
-                left_cursor = left_cursor + DIRECTIONS.LEFT
+            if under_left_grid_item in '~#':
                 grid[(left_cursor.to_yx_tuple())] = '|'
+                COUNT_WATER += 1
+                left_cursor = left_cursor + DIRECTIONS.LEFT.value
                 continue
             
             elif under_left_grid_item == '.':
-                new_cursors.append(left_cursor + DIRECTIONS.LEFT)
+                new_cursors.append(left_cursor)
                 break
 
     while True:
         right_grid_item = grid[right_cursor.to_yx_tuple()]
 
-        # Wall
+        if right_grid_item == '|':
+            right_cursor = right_cursor + DIRECTIONS.RIGHT.value
+            continue
+
+        # Wall or existing water
         if right_grid_item == '#':
             break
 
         # Open space
         if right_grid_item == '.':
-            under_right_grid_item = grid[(right_cursor + DIRECTIONS.DOWN).to_yx_tuple()]
+            under_right_grid_item = grid[(right_cursor + DIRECTIONS.DOWN.value).to_yx_tuple()]
 
-            if under_right_grid_item == '~':
-                right_cursor = right_cursor + DIRECTIONS.RIGHT
+            if under_right_grid_item in '~#':
                 grid[(right_cursor.to_yx_tuple())] = '|'
+                COUNT_WATER += 1
+                right_cursor = right_cursor + DIRECTIONS.RIGHT.value
                 continue
             
             elif under_right_grid_item == '.':
-                new_cursors.append(right_cursor + DIRECTIONS.RIGHT)
+                new_cursors.append(right_cursor)
                 break
     
+    if len(new_cursors) == 0:
+        left_x = cursor.x
+        while True: 
+            left_x -= 1
+            if grid[(cursor.y, left_x)] == '#':
+                break
+            
+        right_x = cursor.x
+        while True:
+            right_x += 1
+            if grid[(cursor.y, right_x)] == '#':
+                break
+
+        for i in range(left_x + 1, right_x):
+            grid[(cursor.y, i)] = '~'
+
     return new_cursors
-    
-print_grid(grid)
 
 queue = Queue()
-queue.put(Vector2(500, 0))
+queue.put(Vector2(500, 1))
 while not queue.empty():
 
     cursor = queue.get()
+    # print_grid_around_cursor(grid, cursor)
+    # time.sleep(1)
     while True:
-        if grid[cursor.to_yx_tuple()] == '.':
-            grid[cursor.to_yx_tuple()] == '|'
+        if cursor.y > ymax:
+            break
 
-        peek_down = cursor + DIRECTIONS.DOWN
+        if grid[cursor.to_yx_tuple()] == '|':
+            break
+
+        if grid[cursor.to_yx_tuple()] == '.':
+            grid[cursor.to_yx_tuple()] = '|'
+            COUNT_WATER += 1
+            # print_grid(grid)
+
+        peek_down = cursor + DIRECTIONS.DOWN.value
         
         if grid[peek_down.to_yx_tuple()] == '.':
             cursor = peek_down
             continue
         
-        elif grid[peek_down.to_yx_tuple()] == '#':
+        elif grid[peek_down.to_yx_tuple()] in '~#':
             new_cursors = []
             
             while len(new_cursors) == 0:
                 new_cursors = fill_left_right_get_new_cursors(grid, cursor)
-                cursor = cursor + DIRECTIONS.UP
+                cursor = cursor + DIRECTIONS.UP.value
+
+                if len(new_cursors) == 0 and grid[cursor.to_yx_tuple()] != '|':
+                    grid[cursor.to_yx_tuple()] = '|'
+
+                # print_grid(grid)
 
             for new_cursor in new_cursors:
                 queue.put(new_cursor)
-
             
-print_grid(grid)
+            # Move onto next cursor
+            break
+
+print(COUNT_WATER)
