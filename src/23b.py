6ee57adb813f0c7354b90949a12b6220b2c45430
Andrew Barnett <andrew@legacyesports.gg>
2018-12-23 20:20:44 -0600

    23b

--- src/23b.py
+++ src/23b.py
@@ -6,6 +6,9 @@ class Nanobot:
     def __init__(self, pos: Vector3, radius):
         self.pos = pos
         self.radius = radius
+    
+    def __floordiv__(self, amt):
+        return Nanobot(Vector3(self.pos.x // amt, self.pos.y // amt, self.pos.z // amt), self.radius // amt)
 
 inp = FileImporter.get_input("/../input/23.txt").split("\n")
 nanobots = []
@@ -16,17 +19,34 @@ for i in inp:
 max_in_range = -1
 max_in_range_coords = []
 
-for x in range(min(nanobots, key = lambda x: x.pos.x).pos.x, max(nanobots, key = lambda x: x.pos.x).pos.x + 1):
-    for y in range(min(nanobots, key = lambda x: x.pos.y).pos.y, max(nanobots, key = lambda x: x.pos.y).pos.y + 1):
-        for z in range(min(nanobots, key = lambda x: x.pos.z).pos.z, max(nanobots, key = lambda x: x.pos.z).pos.z + 1):
-            current_pos = Vector3(x, y, z)
-            num_in_range = sum(1 for i in nanobots if i.pos.manhattan_distance(current_pos) <= i.radius)
-            
-            if num_in_range > max_in_range:
-                max_in_range_coords = [current_pos]
-                max_in_range = num_in_range
+minx, maxx = min(nanobots, key = lambda x: x.pos.x).pos.x, max(nanobots, key = lambda x: x.pos.x).pos.x
+miny, maxy = min(nanobots, key = lambda x: x.pos.y).pos.y, max(nanobots, key = lambda x: x.pos.y).pos.y
+minz, maxz = min(nanobots, key = lambda x: x.pos.z).pos.z, max(nanobots, key = lambda x: x.pos.z).pos.z
+div = 10000000
+while True:
+    for x in range(minx, maxx + 1, div):
+        for y in range(miny, maxy + 1, div):
+            for z in range(minz, maxz, div):
+                current_pos = Vector3(x, y, z)
+                nbs = [nanobot // div for nanobot in nanobots]
+                num_in_range = sum(1 for i in nanobots if i.pos.manhattan_distance(current_pos) <= i.radius)
+                
+                if num_in_range > max_in_range:
+                    max_in_range_coords = [current_pos]
+                    max_in_range = num_in_range
 
-            elif num_in_range == max_in_range:
-                max_in_range_coords.append(current_pos)
+                elif num_in_range == max_in_range:
+                    max_in_range_coords.append(current_pos)
 
-print(min(i.manhattan_distance(Vector3(0, 0, 0)) for i in max_in_range_coords))
+    if div == 1:
+        print(min([i.manhattan_distance(Vector3(0,0,0)) for i in max_in_range_coords]))
+        break
+
+    else:
+        shortest_distance_coord = min(max_in_range_coords, key = lambda x: x.manhattan_distance(Vector3(0,0,0)))
+        minx, maxx = shortest_distance_coord.x - div, shortest_distance_coord.x + div
+        miny, maxy = shortest_distance_coord.y - div, shortest_distance_coord.y + div
+        minz, maxz = shortest_distance_coord.z - div, shortest_distance_coord.z + div
+        div //= 10
+
+        
